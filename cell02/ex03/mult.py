530ef3fb6534fbce4719a8a535011fc146c317e6
Bannasorn Lalod <Banna.lalod@gmail.com>
2026-02-09 16:45:40 +0700

    cell02

---
+++ cell02/ex03/mult.py
@@ -0,0 +1,9 @@
+first = int(input("Enter the first number: "))
+second = int(input("Enter the second number: "))
+result = first * second
+if result > 0:
+    print("The result is positive.")
+elif result < 0:
+    print("The result is negative.")
+else:
+    print("The result is zero.")
