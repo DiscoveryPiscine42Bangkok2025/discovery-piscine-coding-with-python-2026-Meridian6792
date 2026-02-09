ec692b6516f655215aba3dea76d197a63f6d173a
Bannasorn Lalod <Banna.lalod@gmail.com>
2026-02-09 16:46:19 +0700

    cell02f

---
+++ cell02/ex00/iszero.py
@@ -0,0 +1,5 @@
+i = int(input())
+if i == 0:
+    print("This number is equal to zero")
+else:
+    print("This number is different to zero")
---
+++ cell02/ex01/isneg.py
@@ -0,0 +1,5 @@
+i = int(input())
+if i < 0:
+    print("This number is negative.")
+else:
+    print("This number is positive.")
